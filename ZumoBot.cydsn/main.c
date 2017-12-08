da46bd003229fa01ce35db974b81307b66a65ab5
Vedran Brnjetic <vedran.brnjetic@metropolia.fi>
2017-12-08 12:37:03 +0200

    The big code cleanup

--- ZumoBot.cydsn/main.c
+++ ZumoBot.cydsn/main.c
@@ -52,36 +52,31 @@ int rread(void);
  * @details  ** You should enable global interrupt for operating properly. **<br>&nbsp;&nbsp;&nbsp;CyGlobalIntEnable;<br>
 */
 
-int turn_rate(int speed, int a, int break_factor, int min, int max){
-    int x;
-    x = (int)( (float)speed / (1.5 - (1 - (( (float)max / min) / 8))) ) - 
-    (int)(a * break_factor * ((float)max / min));
-    
-    if(x > 255) x = 255;
-    if(x < 0) x = 0;
-    
-    return x;
-}
-
+//The function that checks for the stop lines. 
+//It also checks if the line is lost and backs the robot for a bit
+//l and r are raw sensor values
+//lm and rm are left and right middle sensors raw values 
+//flag is a pointer to an external flag defined in the main function outside of the loop
+//stop is a pointer to an external stop variable defined in the main function, it is used as a stop condition
 void should_I_stop(int l, int r, int lm, int rm, int * flag, int * stop){
+    //I used 16000 for baseline black, a little bit conservative but doesn't fail
     if(l > 16000 && r > 16000 && * flag == 0) {
         * flag = * flag + 1; 
         * stop = * stop + 1;
     }
     else if(l < 16000 && r < 16000 && * flag == 1){
         * flag = * flag - 1;
-    }
+    }//why am I using else if here?
     else if(rm < 5000 && lm < 5000){
             motor_backward(155, 10);
             motor_forward(155, 1);
      
     }
 }
-///Serious attempt
+
 int main(void){
 
     struct sensors_ ref;
-    //struct sensors_ dig;
     CyGlobalIntEnable; 
     UART_1_Start();
   
@@ -97,14 +92,12 @@ int main(void){
         reflectance_read(&ref);
     }
     
-    
-    
     ///drive up to the first line
     motor_start();
     do{
         motor_forward(100, 5);
         reflectance_read(&ref);
-    }while( ref.l3 < 15000 && ref.r3 < 15000);
+    }while( ref.l3 < 15000 && ref.r3 < 15000); //15000 as a conservative failsafe value
     
     motor_stop();
     //*/
@@ -115,81 +108,55 @@ int main(void){
     
     //*/
     
-    ///GO!
+    //Initialization - stop and flag - race control
     int stop = 0;
     int flag = 1;
+    
+    //KP, KD, Sensor value scaling to the motor control range
     float error = 0, lastError = 0;
     
-    long int i=0;
-    
-    float x=0.0013; //scaling factor
+    float x=0.0013; //scaling factor because I work with raw sensor data
     float kp = 39;
     float kd = 680;
     float PV = 0;
-    //float minErrDiff=0;
-    //float maxErrDiff=0;
     
+    //Main tunning loop
     while( stop<=1){    
         
             do{
-                
+                //read the sensors
                 reflectance_read(&ref);
+                //check if we need to fix lost line or stop at the Finnish line
                 should_I_stop(ref.l3, ref.r3, ref.l1, ref.r1, &flag, &stop);                
                 
+                
                 error = (float)((ref.l1 - ref.r1) / 2);
+                
+                //keep the error at maximum if the line is lost to either side
                 if(ref.l1 > ref.r1 && ref.l1 < 18000){
                     error = 9000;
                 }
                 if(ref.r1 > ref.l1 && ref.r1 < 18000){
                     error = -9000;
                 }
-                
-                
-                //kp between 95 and 125 works smooth
-                /*//remove from production - a testing setup to tune the kp in increments of 5
-                i++;
-                if(i % 2000 == 0){
-                    motor_stop();
-                    while(!(get_IR())){}
-                    motor_start();
-                    kd += 5;
-                    i = 0;
-                }
-                printf("%f\n", kp);
-                //*/
-                
+                 
                 PV = x * (kp * error + (kd * (error - lastError)));
                 
                 float left, right;
-                //CyDelay(500);
+             
                 
-                    left = 234 - PV;
-                    right = 234 + PV;
+                left = 234 - PV;
+                right = 234 + PV;
                 
                 
                 if(left>255) left = 234;
                 if(right>255) right = 234;
                 if(left<0) left = 0;
                 if(right<0) right = 0;
-                //motor_stop();
-                //float errDiff = error - lastError;
-                /*
-                if(errDiff < 0){
-                    if(-errDiff < minErrDiff) minErrDiff = -errDiff;
-                    if(-errDiff > maxErrDiff) maxErrDiff = -errDiff;
-                }
-                else{
-                    if(errDiff < minErrDiff) minErrDiff = errDiff;
-                    if(errDiff > maxErrDiff) maxErrDiff = errDiff;
+             
+                lastError = error;
                 
-                }
-                printf("%f\t%f\t%f\t%f\t%f\t%f\n", left, right, PV, errDiff, minErrDiff, maxErrDiff);
-                */
-                lastError = error;///
-                motor_turn(
-                    left, 
-                    right,
-                     1);///*/
+                motor_turn(left, right, 1);
                 
             }while(stop<=1);
             
